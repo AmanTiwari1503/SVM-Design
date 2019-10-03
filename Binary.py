8b7a9084015f92d6646110ed25355b6c328369dd
AmanTiwari1503 <tiwariaman2000@gmail.com>
2019-10-03 10:34:22 +0530

    Made minor changes

--- Binary.py
+++ Binary.py
@@ -10,47 +10,53 @@ num_features = 10
 F = Dataset.iloc[:,:num_features].values
 T = Dataset.iloc[:,-1].values
 
-cr_batches = 10 
+cr_batches = 6
+
+label1 = 0
+label2 = 1
+F1 = []
+T1 = []
+for k in range(0,len(T)):
+	if(T[k]==label1):
+		F1.append(F[k])
+		T1.append(0)
+	elif(T[k]==label2):
+		F1.append(F[k])
+		T1.append(1)
+F1 = np.asarray(F1)
+T1 = np.asarray(T1)
 
 def conventionalMethod(X,Y):
-	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30,random_state=109)
-	classifier = svm.SVC(kernel = 'linear')
+	X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.30,random_state=100)
+	classifier = svm.SVC(kernel = 'rbf',gamma=0.1,C=1)
 	classifier.fit(X_train,y_train)
 	y_pred = classifier.predict(X_test)
 	accuracy = metrics.accuracy_score(y_test,y_pred)
-#	precision = metrics.precision_score(y_test,y_pred)
-#	recall = metrics.recall_score(y_test,y_pred)	
-#	print("Conventional method:")
-#	print("Accuracy = "+str(accuracy))
-#	print("Precision = "+str(precision))
-#	print("Recall = "+str(recall))
-#	print("\n")
-	return accuracy
+	cm = metrics.confusion_matrix(y_test,y_pred)
+	precision = metrics.precision_score(y_test,y_pred)
+	recall = metrics.recall_score(y_test,y_pred)	
+	return accuracy,cm,precision,recall
 
 def cross_validation(X,Y):
 	accuracy_cr = 0
 	precision_cr = 0
 	recall_cr = 0
+	cm_cr = np.zeros((2,2))
 	cr_batch_size = int(X.shape[0]/cr_batches)
 	for k in range(0,cr_batches):
 		X_test = X[k*cr_batch_size:(k+1)*cr_batch_size,:]
 		X_train = np.delete(X, np.s_[k*cr_batch_size:(k+1)*cr_batch_size],0)
 		y_test = Y[k*cr_batch_size:(k+1)*cr_batch_size]
 		y_train = np.delete(Y, np.s_[k*cr_batch_size:(k+1)*cr_batch_size])
-		classifier = svm.SVC(kernel = 'linear')
+		classifier = svm.SVC(kernel = 'rbf',gamma = 1,C=1)
 		classifier.fit(X_train,y_train)
 		y_pred = classifier.predict(X_test)
 		accuracy_cr = accuracy_cr + metrics.accuracy_score(y_test,y_pred)
 		precision_cr = precision_cr + metrics.precision_score(y_test,y_pred)
 		recall_cr = recall_cr + metrics.recall_score(y_test,y_pred)
-	print("Cross validation method:")
-	print("Accuracy = "+str(accuracy_cr/cr_batches))
-	print("Precision = "+str(precision_cr/cr_batches))
-	print("Recall = "+str(recall_cr/cr_batches))
-	print("\n")
+		cm_cr = cm_cr + metrics.confusion_matrix(y_test,y_pred)
+	return accuracy_cr/cr_batches,cm_cr/cr_batches,precision_cr/cr_batches,recall_cr/cr_batches
 	
-#conventionalMethod(F1,T1)
-#cross_validation(F1,T1)
 def best_label_pair():
 	accuracy_mat = []
 	label_mat = range(0,10)
@@ -72,9 +78,9 @@ def best_label_pair():
 					T1.append(1)
 			F1 = np.asarray(F1)
 			T1 = np.asarray(T1)	
-			accuracy_mat1.append(conventionalMethod(F1,T1))
+			accuracy_mat1.append(conventionalMethod(F1,T1)[0])
 #			print(i,j)
-		tmp = min(accuracy_mat1)
+		tmp = max(accuracy_mat1)
 		tmp_in = accuracy_mat1.index(tmp)
 		if(tmp_in>=i):
 			tmp_in = tmp_in+1
@@ -82,5 +88,23 @@ def best_label_pair():
 		accuracy_mat.append(tmp)
 	plt.plot(label_mat,accuracy_mat)
 	plt.show
+
+def print_params_conv(X,Y):
+	params = conventionalMethod(X,Y)
+	print("Conventional method:")
+	print("Accuracy = "+str(params[0]))
+	print(params[1])
+	print("Precision = "+str(params[2]))
+	print("Recall = "+str(params[3]))
+
+def print_params_cr(X,Y):
+	params = cross_validation(X,Y)
+	print("Cross validation method:")
+	print("Accuracy = "+str(params[0]))
+	print(params[1])
+	print("Precision = "+str(params[2]))
+	print("Recall = "+str(params[3]))
 	
-best_label_pair()
+#print_params_conv(F1,T1)
+print_params_cr(F1,T1)
+#best_label_pair()
